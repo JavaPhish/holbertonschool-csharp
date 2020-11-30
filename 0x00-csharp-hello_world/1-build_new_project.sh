cb50c0d7d208eb5454ca9f6d102d247927c6bf6e
JavaPhish <cclements3150@gmail.com>
2020-11-30 18:24:43 -0500

    Creates new and builds project

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# Builds a dotnet template in 0-new_project folder
+# Builds a dotnet template in 1-new_project folder
 dotnet new console -n 1-new_project
 dotnet build 1-new_project

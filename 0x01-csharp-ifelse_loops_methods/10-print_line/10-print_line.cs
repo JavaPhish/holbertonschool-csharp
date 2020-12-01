05254464e888e10bc72779004d892c899a988adb
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 18:28:11 -0500

    [Create] Prints a line at specified length

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        for (int x = 0; x < length; x++)
+            Console.Write("_");
+        Console.Write("\n");
+    }
+}

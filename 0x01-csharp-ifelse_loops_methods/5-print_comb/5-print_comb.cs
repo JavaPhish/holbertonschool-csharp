134c56ce2ca8ce406c8aa42db81a7e4195a70c4b
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 18:05:56 -0500

    [Fix] Added newline at end

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -5,8 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("00");
-        for (int num = 1; num < 100; num++)
-            Console.Write($", {num:00}");
+        for (int num = 0; num < 99; num++)
+            Console.Write($"{num:00}, ");
+        Console.Write("99\n");
     }
 }

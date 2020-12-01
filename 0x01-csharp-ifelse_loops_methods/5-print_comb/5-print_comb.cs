0a3fcbfef976fadef58d162998319cca664bed62
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 18:02:58 -0500

    [Create] Prints 00 - 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("00");
+        for (int num = 1; num < 100; num++)
+            Console.Write($", {num:00}");
+    }
+}

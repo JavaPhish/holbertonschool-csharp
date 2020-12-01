950e1cc08347d8ca05c469b0013f31e2bf9ca127
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 17:28:25 -0500

    More practice

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int last = number % 10;
+        Console.Write($"The last digit of {number} is {last}");
+        if (last > 5)
+            Console.Write(" and is greater than 5\n");
+        else if (last == 0)
+            Console.Write(" and is 0\n");
+        else
+            Console.Write(" and is less than 6 and not 0\n");
+    }
+}

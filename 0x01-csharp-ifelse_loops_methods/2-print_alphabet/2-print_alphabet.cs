b8cb19d8a45a40cdc8fb8747186ed04731b487b3
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 17:38:37 -0500

    [Create] Prints alphabet using ascii and write

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int ascii = 97; ascii < 123; ascii++)
+            Console.Write((char)ascii);
+    }
+}

1fc50ec5d323bef85f9945df3b18305d1c27d8cc
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 17:34:39 -0500

    Forgot to change .net version

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -7,12 +7,11 @@ class Program
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
         int last = number % 10;
-        Console.Write($"The last digit of {number} is {last}");
         if (last > 5)
-            Console.Write(" and is greater than 5\n");
+            Console.WriteLine($"The last digit of {number} is {last} and is greater than 5");
         else if (last == 0)
-            Console.Write(" and is 0\n");
+            Console.WriteLine($"The last digit of {number} is {last} and is 0");
         else
-            Console.Write(" and is less than 6 and not 0\n");
+            Console.WriteLine($"The last digit of {number} is {last} and is less than 6 and not 0");
     }
 }

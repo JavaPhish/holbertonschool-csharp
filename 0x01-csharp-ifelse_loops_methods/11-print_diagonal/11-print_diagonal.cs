d60ea7f4160dd87574d296428892a52401b8fede
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 18:34:11 -0500

    [Create] Prints a diagonal line at sepcified length

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int x = 0; x < length; x++) {
+            for (int y = 0; y < x; y++){
+                Console.Write(" ");
+            }
+            Console.Write("\\\n");
+        }
+    }
+}

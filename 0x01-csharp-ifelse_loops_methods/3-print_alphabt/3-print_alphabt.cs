89232299d3757017d05085cc0101f9929af1cc95
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 17:41:28 -0500

    [Create] Prints a - z without q and e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int ascii = 97; ascii < 123; ascii++)
+            if (ascii != 113 && ascii != 101)
+                Console.Write((char)ascii);
+    }
+}
+

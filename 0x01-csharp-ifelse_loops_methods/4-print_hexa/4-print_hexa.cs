1c7bc4ce4148d5bbfb0677eacfd19d839a89d0a7
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 17:50:18 -0500

    [Create] Prints 0 - 98 in hex

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num = 0; num <= 98; num++)
+            Console.WriteLine("{0} = 0x{1}", num, num.ToString("X"));
+    }
+}

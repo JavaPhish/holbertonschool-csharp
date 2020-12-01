541c499fdfb7b54bd6f7f3a6d3d1bc23ec63ecaf
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 18:41:57 -0500

    [Create] Its just fizz buzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write('1');
+            for (int num = 2; num < 101; num++)
+            {
+                if (num % 15 == 0)
+                    Console.Write(" FizzBuzz");
+                else if (num % 5 == 0)
+                    Console.Write(" Buzz");
+                else if (num % 3 == 0)
+                    Console.Write(" Fizz");
+                else
+                    Console.Write($" {num}");
+            }
+            Console.Write("\n");
+        }
+    }
+}

b68b3b8bd88332be7772b9dde9c08e5305beed0a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 17:52:16 -0500

    [Fix] They wanted it lower case.

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         for (int num = 0; num <= 98; num++)
-            Console.Write("{0} = 0x{1}\n", num, num.ToString("X"));
+            Console.Write("{0} = 0x{1}\n", num, num.ToString("x"));
     }
 }

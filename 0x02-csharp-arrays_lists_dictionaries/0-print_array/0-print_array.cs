d46fd9bfa37250fbb21e5b246f5a84f34cd47801
BuildTools <unconfigured@null.spigotmc.org>
2020-12-02 17:19:45 -0500

    [Create] Creates and prints an array of ints of size 'size'

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        // Create and initialize array
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+           array[i] = i;
+        }
+
+        // Print the array
+        for (int x = 0; x < size; x++)
+        {
+            if (x + 1 != size)
+                Console.Write($"{x} ");
+            else
+                Console.Write($"{x}");
+        }
+        Console.Write("\n");
+
+        return array;
+    }
+}

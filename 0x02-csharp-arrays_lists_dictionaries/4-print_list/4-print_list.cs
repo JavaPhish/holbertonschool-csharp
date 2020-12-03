bb90bfb2f993e3cabd1859782ccbff8bc0735b1a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-02 20:57:57 -0500

    Lists

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> narray = new List<int>();
+        for (int i = 0; i < size; i++)
+           narray.Add(i);
+
+        // Print the array
+        for (int x = 0; x < size; x++)
+            if (x + 1 != size)
+                Console.Write($"{x} ");
+            else
+                Console.Write($"{x}");
+
+        Console.Write("\n");
+
+        return narray;
+    }
+}

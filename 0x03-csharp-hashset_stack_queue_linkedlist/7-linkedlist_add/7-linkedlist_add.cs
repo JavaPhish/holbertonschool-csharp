c64554b3258c0dd98def35147116f6b1de9858ce
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 18:50:58 -0500

    [CREATE] Inserts a new node at the beginning of a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> new_n = new LinkedListNode<int>(n);
+        myLList.AddFirst(new_n);
+
+        return new_n;
+    }
+}

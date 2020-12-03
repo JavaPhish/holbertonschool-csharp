716468ec901d30c005e39863421b71bc7f04afbb
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 18:17:40 -0500

    [CREATE] Some basic stack practice in C#

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        Console.WriteLine($"Top item: {aStack.Peek()}");
+        Console.WriteLine($"Stack contains \"Javascript\": {aStack.Contains(search)}");
+
+        if (aStack.Contains(search))
+        {
+            while (aStack.Peek() != search)
+                aStack.Pop();
+            aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}

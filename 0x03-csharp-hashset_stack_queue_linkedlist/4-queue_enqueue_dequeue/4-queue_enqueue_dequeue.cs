eee236d2d7a9584451ddca387a8642dd460ea1a2
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 18:32:12 -0500

    [CREATE] Basic queue practice in C#

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        Console.WriteLine($"Top item: {aQueue.Peek()}");
+        Console.WriteLine($"Stack contains \"{search}\": {aQueue.Contains(search)}");
+
+        aQueue.Enqueue(newItem);
+
+        if (aQueue.Contains(search))
+        {
+            while (aQueue.Contains(search))
+                aQueue.Dequeue();
+        }
+
+        return aQueue;
+    }
+}

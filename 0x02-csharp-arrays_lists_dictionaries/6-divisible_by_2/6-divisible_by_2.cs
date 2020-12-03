cf162a04c32a3f267d0aec28dea84be4d82274d7
BuildTools <unconfigured@null.spigotmc.org>
2020-12-02 21:16:44 -0500

    [Create] Creates a bool array based on %2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> array = new List<bool>();
+
+        foreach (var item in myList)
+        {
+            if (item % 2 == 0)
+                array.Add(true);
+            else
+                array.Add(false);         
+        }
+
+        return array;
+    }
+}

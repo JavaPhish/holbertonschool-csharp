c32ec72d3532f29426d94721606ed0bc82df9316
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 18:42:48 -0500

    [CREATE] Creates a linked list of given size

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+
+        while (size > 0)
+        {
+            size--;
+            LinkedListNode<int> new_n = new LinkedListNode<int>(size);
+            list.AddFirst(new_n);
+        }
+
+        foreach (var item in list)
+            Console.WriteLine(item);
+
+        return list;
+    }
+}

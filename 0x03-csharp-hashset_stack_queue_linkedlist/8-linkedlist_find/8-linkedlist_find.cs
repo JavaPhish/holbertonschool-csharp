29a4958f279835ce0613c9010bcd2be800bd278b
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 18:55:19 -0500

    [CREATE] Finds the index of a value in a linked list. Theres a better way to do this but they didnt want me to use it

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+
+        foreach (var item in myLList)
+        {
+            if (value == item)
+                return index;
+            index++;
+        }
+
+        return -1;
+    }
+}

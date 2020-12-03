fab45c71b4045ab4e541ffb153b4c365d875674c
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 13:46:40 -0500

    Created

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		List<int> used_n = new List<int>();
+		int sum = 0;
+
+		foreach (var num in myList)
+		{
+			if (used_n.Contains(num))
+				continue;
+
+			sum += num;
+			used_n.Add(num);
+		}
+		return sum;
+	}
+}
+

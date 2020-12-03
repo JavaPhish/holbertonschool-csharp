a3fc93a4c23f7e8608afbe542b0e771ae6ebbd74
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 17:57:41 -0500

    [CREATE] Finds all different values between two collections using hash tables

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> hash1 = new HashSet<int>();
+        HashSet<int> hash2 = new HashSet<int>();
+
+        /* Create two hash tables and intersect them.
+            this gives us only the values that dont
+            exist in both */
+        foreach (var item in list1)
+            hash1.Add(item);
+        foreach (var item in list2)
+            hash2.Add(item);
+
+        hash1.IntersectWith(hash2);
+
+        /* Convert it to an int collection */
+        List<int> myList = new List<int>();
+        foreach (var item in hash1)
+            myList.Add(item);
+
+        return (myList);
+    }
+}

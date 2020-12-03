3c1cfb4a4f96e3dd1c5dfe4fc79df0e7480d3eb2
BuildTools <unconfigured@null.spigotmc.org>
2020-12-02 22:19:23 -0500

    Temp

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        var maxValue = dict.Values.Max();
+        return mylist.GetByValueKey(maxValue);
+    }
+}

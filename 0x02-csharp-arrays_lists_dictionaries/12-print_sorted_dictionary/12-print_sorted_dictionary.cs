d3af5eeaa7abbefb5b9db4b0fa17c2ec0b4b2699
BuildTools <unconfigured@null.spigotmc.org>
2020-12-02 22:01:31 -0500

    Pushing just to save these

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        Dictionary<string, string> newDict = new Dictionary<string, string>();
+ 
+        string sorted_keys[] = newDict.Keys.ToList();
+        list.Sort();
+
+        foreach (var item in list)
+        {
+            newDict.Add(list, myDict[list]);
+        }
+
+        return newDict;
+    }
+}

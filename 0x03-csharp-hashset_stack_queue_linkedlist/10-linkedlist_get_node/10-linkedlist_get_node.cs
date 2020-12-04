4490affa96189d2b9e35ef53d5e4111aae6f3491
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 19:12:28 -0500

    [CREATE] Finds the value of a node at index in a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {        
+        int index = 0;
+        LinkedListNode<int> node = myLList.First;
+
+        while (node.Next != null)
+        {
+            if (index == n) {
+                return node.Value;
+            }
+            node = node.Next;
+            index++;
+        }
+
+        return 0;
+    }
+}

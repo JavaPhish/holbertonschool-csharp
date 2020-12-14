90bb1bab1bd13fa35aa1fe92acb8a96d0c429cdc
BuildTools <unconfigured@null.spigotmc.org>
2020-12-14 13:43:48 -0500

    Try catch practice

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+
+        int result = 0;
+        try
+        {
+            result = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            result = 0;
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+    }
+}

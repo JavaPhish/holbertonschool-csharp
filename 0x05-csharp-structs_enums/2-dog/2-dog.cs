389dac57e2d97daff0366a9557c5997c03cd08c6
BuildTools <unconfigured@null.spigotmc.org>
2020-12-14 13:18:45 -0500

    Added a constructor to 1-dog

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public Dog(string Name, float Age, string Owner, Rating rating1)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = rating1;
+    }
+
+    public string name { get; }
+    public float age { get; }
+    public string owner { get; }
+    public Rating rating { get; }
+}

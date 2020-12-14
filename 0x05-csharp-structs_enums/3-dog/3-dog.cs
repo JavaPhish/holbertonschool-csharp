269de014c72ef16aa9e7e585a623980ebc186058
BuildTools <unconfigured@null.spigotmc.org>
2020-12-14 13:24:12 -0500

    Override toString in struct

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public Dog(string Name, float Age, string Owner, Rating rating1)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = rating1;
+    }
+
+   public override string ToString()
+   {
+      return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+   }
+
+    public string name { get; }
+    public float age { get; }
+    public string owner { get; }
+    public Rating rating { get; }
+}

7e1f8ada7ffb92a29a45775765ee2cb36315e1a1
BuildTools <unconfigured@null.spigotmc.org>
2020-12-15 14:38:44 -0500

    Added Name property/private field, Based on 3-enemy

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty Zombie class
+    /// </summary>
+    class Zombie
+    {
+        public int health;
+        string name = "(No name)";
+
+        /// <summary>
+        /// Sets the health of the zombie to 0 (Constructor)
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Sets the health of the zombie to value (Public Constructor)
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary>
+        /// Health property
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Gets the zombies health (Getter)
+        /// </summary>
+        /// <returns>
+        /// The zombies current health
+        /// </returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}

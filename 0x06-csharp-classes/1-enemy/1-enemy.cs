13fccaeedd962585202009a0bf93e820838b1361
BuildTools <unconfigured@null.spigotmc.org>
2020-12-15 14:16:54 -0500

    Added constructor (Based on 0)

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty Zombie class
+    /// </summary>
+    class Zombie
+    {
+        public int health;
+
+        /// <summary>
+        /// Sets the health of the zombie to 0 (Constructor)
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;  
+        }
+    }
+}

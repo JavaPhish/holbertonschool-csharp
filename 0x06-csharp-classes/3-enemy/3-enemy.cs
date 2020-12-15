9451ab19298bcbb8daf78a0ada01a078d9cf10a5
BuildTools <unconfigured@null.spigotmc.org>
2020-12-15 14:28:12 -0500

    Added a getter for health, Based on 2-enemy

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty Zombie class
+    /// </summary>
+    class Zombie
+    {
+        public int health;
+
+        /// <summary>
+        /// Sets the health of the zombie to 0 (Constructor)
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;  
+        }
+
+        /// <summary>
+        /// Sets the health of the zombie to value (Public Constructor)
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary>
+        /// Gets the zombies health (Getter)
+        /// </summary>
+        /// <returns>
+        /// The zombies current health
+        /// </returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}

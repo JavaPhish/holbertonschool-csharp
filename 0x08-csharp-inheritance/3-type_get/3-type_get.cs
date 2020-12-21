b40f6f7252f98506f98fa1d28ff04e30813349ed
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 17:20:25 -0500

    Prints all properties and methods of a given object

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Base obj class
+/// </summary>
+class Obj
+{
+
+    /// <summary>
+    /// Prints all properties and methods of a given object
+    /// </summary>
+    public static void Print(object myObj)
+    {
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach (var prop in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine($"{prop.Name}");
+        }
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (var prop in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine($"{prop.Name}");
+        }
+    }
+}

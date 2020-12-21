24d15efc4118bd93f5845cbbc4d753a669452ea4
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 13:49:19 -0500

    Checks if instance of array

---
+++ 0x08-csharp-inheritance/1-is/1-instance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Base obj class
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if an object is an array
+    /// </summary>
+    /// <returns>
+    /// True if array, false otherwise
+    /// </returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType().IsArray)
+        {
+            return true;
+        }
+        return false;
+    }
+}

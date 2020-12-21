302d9ea5330fb50dad9563e701a609291b0a95a8
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 13:02:21 -0500

    Checks is an object is an int

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Base obj class
+/// </summary>
+class Obj
+{
+        /// <summary>
+        /// Checks if an object is an int
+        /// </summary>
+        /// <returns>
+        /// True if int, false otherwise
+        /// </returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+        {
+            return true;
+        }
+        return false;
+    }
+}

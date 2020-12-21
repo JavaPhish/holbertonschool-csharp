1380883b6966313b7872f406a46ab0dca8106993
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 14:05:16 -0500

    CHecks if a type is a subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Base obj class
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if an object is a sublass of basetype
+    /// </summary>
+    /// <returns>
+    /// True if subclass, false otherwise
+    /// </returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (baseType.IsSubclassOf(derivedType))
+        {
+            return true;
+        }
+        return false;
+    }
+}

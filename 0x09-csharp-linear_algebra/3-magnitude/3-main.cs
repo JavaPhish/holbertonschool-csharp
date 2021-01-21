394c31f05ee9096bc3e63b89f5e1cefe4e3f145c
Carter C <cclements3150@gmail.com>
2021-01-21 10:20:50 -0500

    Finds the magnitude of a given 2d or 3d vector

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] test = new double[] {7, -3, -9};
+        Console.WriteLine(VectorMath.Magnitude(test));
+    }
+}

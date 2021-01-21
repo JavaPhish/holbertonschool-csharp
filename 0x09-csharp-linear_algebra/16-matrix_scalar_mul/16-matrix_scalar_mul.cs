5dd0251667e77263f1e06517030cd9f6b446e406
Carter C <cclements3150@gmail.com>
2021-01-21 13:41:08 -0500

    Basic scalar operation

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+ 
+        if (matrix.Length == 2 || matrix.Length == 3)
+        {
+
+            int uBound0 = matrix.GetUpperBound(0);
+            int uBound1 = matrix.GetUpperBound(1);
+            for (int x = 0; x < uBound0; x++)
+            {
+                for (int y = 0; y < uBound1; y++)
+                {
+                    matrix[x, y] = matrix[x, y] * scalar;
+                }
+            }
+            return matrix;    
+        }
+
+        double[,] fail_sum = new double[,] {{-1}};
+
+        return fail_sum;   
+    }
+}

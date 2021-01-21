e084b5f27eac37dc135bd3663e1473c8e496c115
Carter C <cclements3150@gmail.com>
2021-01-21 10:51:21 -0500

    Basic vector scaler operation

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+
+        if (vector.Length == 2)
+        {
+            double[] vector_mul = new double[2];
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector_mul[i] = (vector[i] * scalar);
+            }
+
+            return vector_mul;
+        }
+
+        if (vector.Length == 3)
+        {
+            double[] vector_mul = new double[3];
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector_mul[i] = (vector[i] * scalar);
+            }
+
+            return vector_mul;
+        }
+
+        double[] fail_mul = new double[] {-1};
+
+        return fail_mul;
+    }
+}

bf351dd469dbb4b59652dfc58f787b12f905b995
Carter C <cclements3150@gmail.com>
2021-01-21 10:43:02 -0500

    Adds two vectors (3d or 2d)

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length == vector2.Length)
+        {
+            if (vector1.Length == 2)
+            {
+                double[] vector_sum = new double[2];
+
+                for (int i = 0; i < vector1.Length; i++)
+                {
+                    vector_sum[i] = (vector1[i] + vector2[i]);
+                }
+
+                return vector_sum;
+            }
+
+            if (vector1.Length == 3)
+            {
+                double[] vector_sum = new double[3];
+
+                for (int i = 0; i < vector1.Length; i++)
+                {
+                    vector_sum[i] = (vector1[i] + vector2[i]);
+                }
+
+                return vector_sum;
+            }
+        }
+
+        double[] fail_sum = new double[] {-1};
+
+        return fail_sum;
+    }
+}

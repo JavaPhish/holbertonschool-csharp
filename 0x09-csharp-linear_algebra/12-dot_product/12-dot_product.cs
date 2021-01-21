2098b175af6eabc6ffdd2abe6c6c3c968af30c7f
Carter C <cclements3150@gmail.com>
2021-01-21 11:23:58 -0500

    dotnet product of two 2d or 3d vectors

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length == vector2.Length)
+        {
+            if (vector1.Length >= 2)
+            {
+                double sum = 0;
+
+                for (int i = 0; i < vector1.Length; i++)
+                {
+                    sum += (vector1[i] + vector2[i]);
+                }
+
+                return vector_sum;
+            }
+        }
+        return -1;
+    }
+}

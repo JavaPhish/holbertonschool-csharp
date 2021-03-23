b536a22f1a8eac0fa016b07021f34ef7527a9a50
Carter C <cclements3150@gmail.com>
2021-03-22 20:42:28 -0400

    Added dequeue

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+/// <summary>
+/// Base queue
+/// </summary>
+public class Queue<T>
+{
+    Node head = null;
+    Node tail = null;
+    int count = 0;
+
+    /// <summary>
+    /// Returns this queues type
+    /// </summary>
+    public Type CheckType() {
+        return (typeof(T));
+    }
+
+    /// <summary>
+    /// Node
+    /// </summary>
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Node(T n)
+        {
+            value = n;
+        }
+    }
+
+    /// <summary>
+    /// Dequeue
+    /// </summary>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        T v = head.value;
+        Node old = head;
+        head = head.next;
+        old.next = null;
+
+        return v;
+    }
+
+    /// <summary>
+    /// Adds a new node
+    /// </summary>
+    public Node Enqueue(T n)
+    {
+        if (head == null)
+        {
+            head = new Node(n);
+        }
+        else
+        {
+            Node new_node = new Node(n);
+            new_node.next = head;
+
+            head = new_node;
+        }
+
+        count = 1;
+        tail = head;
+        while (tail.next != null)
+        {
+            tail = tail.next;
+            count++;
+        }
+
+        return head;
+    }
+
+
+
+    /// <summary>
+    /// Returns the node count
+    /// </summary>
+    public int Count()
+    {
+        return count;
+    }
+}

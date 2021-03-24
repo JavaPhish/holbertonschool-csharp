d34ed2a85f52267c52a678c65fa92399da69a724
Carter C <cclements3150@gmail.com>
2021-03-23 20:45:47 -0400

    Base abstract class

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// Base class
+abstract class Base
+{
+    /// The name
+    public string name;
+
+    /// ToString override
+    public override string ToString()
+    {
+        return name + " is a " + this.GetType();
+    }
+}

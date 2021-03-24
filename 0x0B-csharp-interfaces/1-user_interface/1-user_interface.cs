c7bfadc4d2a6945d544b14de2366276dfbd6a4e4
Carter C <cclements3150@gmail.com>
2021-03-23 22:09:00 -0400

    combined files

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -16,3 +16,63 @@ public abstract class Base
         return (name + " is a " + this.GetType());
     }
 }
+
+/// implements things
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    /// Test var
+    public void Break() {}
+    /// Test var
+    public void Collect() {}
+    /// Test var
+    public void Interact() {}
+    
+    /// Test func
+    public int durability
+    {
+        get;
+        set;
+    }
+
+    /// Test func
+    public bool isCollected
+    {
+        get;
+        set;
+    }
+}
+
+/// Interactive
+public interface IInteractive
+{
+    /// Test func
+    void Interact();
+}
+
+/// Ibreakble
+public interface IBreakable
+{
+    /// Test var
+    int durability
+    {
+        get;
+        set;
+    }
+
+    /// Test func
+    void Break();
+}
+
+/// collectable
+public interface ICollectable
+{
+    /// Test var
+    bool isCollected
+    {
+        get;
+        set;
+    }
+
+    /// test func
+    void Collect();
+}

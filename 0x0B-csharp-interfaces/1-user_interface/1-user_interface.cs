bab49f9372703c6d7f9cfb666491ed3c98f4492c
Carter C <cclements3150@gmail.com>
2021-03-23 21:57:28 -0400

    User interface test interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// Base class
+public abstract class Base
+{
+    /// The name
+    public string name;
+
+    /// ToString override
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType());
+    }
+}

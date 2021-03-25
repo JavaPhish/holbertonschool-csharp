d85eff6a5dc908539af42c207837b1b31b36ac90
Carter C <cclements3150@gmail.com>
2021-03-25 18:46:56 -0400

    Base player class

---
+++ 0x0C-csharp-delegates_events/0-universal_health/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+/// Player object
+public class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+    /// Constructor
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        this.maxHp = maxHp;
+
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+
+        this.hp = maxHp;
+    }
+
+    /// Prints the players health
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health");
+    }
+}
---
+++ 0x0C-csharp-delegates_events/README.md
@@ -0,0 +1,1 @@
+Delegate and event practice

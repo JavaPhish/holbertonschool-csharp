d9ee167a22e25032c7be0f749222f9140fb6beba
Carter C <cclements3150@gmail.com>
2021-04-05 13:51:44 -0400

    Created base class

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Text.Json;
+
+
+/// Base class, other objects will be based on
+public class BaseClass
+{
+    /// ID
+    public string id;
+    /// Date the object was created
+    public DateTime date_created;
+    /// Date the object was updated
+    public DateTime date_updated;
+}
